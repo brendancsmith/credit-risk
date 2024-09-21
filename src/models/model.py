5f7f16220106379d1670d1ff2cd227b723de1a71
Brendan Smith <mail@brendansmith.ai>
2024-09-21 09:57:08 -0500

    Add section titles to the notebook

--- src/models/model.py
+++ src/models/model.py
@@ -2,13 +2,14 @@
 
 from xgboost import XGBClassifier
 
-def create_model():
+def create_model(**kwargs):
     # Initialize model
     xgb_clf = XGBClassifier(
         objective="binary:logistic",
         eval_metric="auc",
         use_label_encoder=False,
         random_state=42,
+        **kwargs,
     )
 
     return xgb_clf
